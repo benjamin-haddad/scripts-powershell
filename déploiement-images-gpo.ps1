f196193a0745ef7c9458082a51ec461b91e37bbc
benjamin-haddad <benjamin-haddad@hotmail.fr>
2025-06-10 18:02:13 +0200

    déploiement-images-gpo.ps1

---
+++ déploiement-images-gpo.ps1
@@ -0,0 +1,32 @@
+# Importation du module GroupPolicy
+Import-Module GroupPolicy
+
+# Définition du nom de la GPO
+$GPOName = "Déploiement Image"
+
+# Création de la GPO
+$GPO = New-GPO -Name $GPOName
+
+# Liaison de la GPO à une unité d'organisation (OU)
+$OU = "OU=Postes,DC=monDomaine,DC=com"
+New-GPLink -Name $GPOName -Target $OU
+
+# Définition du fond d'écran via le registre
+$RegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System"
+$RegistryName = "Wallpaper"
+$ImagePath = "\\serveur\partage\image.jpg"
+
+# Création de la clé si elle n'existe pas
+If (!(Test-Path $RegistryPath)) {
+    New-Item -Path $RegistryPath -Force
+}
+
+New-GPPrefRegistryValue -Name $GPOName -Context User -Key $RegistryPath -ValueName $RegistryName -Type String -Value $ImagePath
+
+# Application de la valeur
+Set-ItemProperty -Path $RegistryPath -Name $RegistryName -Value $ImagePath
+
+# Forcer l’application de la GPO
+gpupdate /force
+
+Write-Output "La GPO '$GPOName' a été créée et configurée avec l’image de fond d’écran."
